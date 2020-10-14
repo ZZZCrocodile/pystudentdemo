3581f391b4df5bcc4456d880e6a37cbf8ac54afb
ZZZCrocodile <461693535@qq.com>
2020-10-14 20:29:38 +0800

    新增名字优化

--- student.py
+++ student.py
@@ -4,3 +4,14 @@ data=[
         'age':13
     }
 ]
+
+#输入名字的优化 （不输入和输入空格的去除）
+def input_name():
+    while True:
+        name = input("请输入名字：").strip() #strip()是去除头尾的空格
+        if name: # 如果有输入名字，就返回名字
+            return name
+        else:
+            continue
+# 选择性别
+def
